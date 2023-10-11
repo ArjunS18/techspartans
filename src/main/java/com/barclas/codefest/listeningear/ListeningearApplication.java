2c2d2b5aea2831e86e9831c0e5f74e9f2911d04d
Arpit Tandon <arpittandon@201.sub-97-40-170.myvzw.com>
2023-10-11 12:41:35 -0400

    intial commit

---
+++ src/main/java/com/barclas/codefest/listeningear/ListeningearApplication.java
@@ -0,0 +1,13 @@
+package com.barclas.codefest.listeningear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListeningearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListeningearApplication.class, args);
+	}
+
+}
